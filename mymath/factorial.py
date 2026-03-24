7841ee4b9d1e23cdc5e63226f95d069e20ef48ec
Nifemi Ogunnowo <ogunnowooluwanifemi@gmail.com>
2026-03-24 10:17:01 +0000

    Added and tested precondition

--- mymath/factorial.py
+++ mymath/factorial.py
@@ -5,6 +5,8 @@ def factorial(n):
     :param int n: The factorial to calculate
     :return: The resultant factorial
     """
+    if n < 0 :
+        raise ValueError("Non real input")
     factorial = 1
     for i in range(1, n + 1):
         factorial = factorial * i
--- tests/test_factorial.py
+++ tests/test_factorial.py
@@ -4,3 +4,13 @@ from mymath.factorial import factorial
 
 def test_3():
     assert factorial(3) == 6
+
+def test_5():
+    assert factorial(5) == 120
+
+def test_0():
+    assert factorial(0) == 1
+    
+def test_negative():
+    with pytest.raises(ValueError):
+        factorial(-1)
